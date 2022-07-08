5e5d763b99006c6cb131dca99963f539a53605a0
jilbosch <jilbosch@icloud.com>
2022-07-08 10:59:52 +0200

    back+front

--- src/main/java/com/proves1/imatges/controllers/MomentControllers.java
+++ src/main/java/com/proves1/imatges/controllers/MomentControllers.java
@@ -4,12 +4,13 @@ import com.proves1.imatges.models.Imatge;
 import com.proves1.imatges.models.Moment;
 import com.proves1.imatges.repositories.IMomentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins="*")
 @RestController
 
 public class MomentControllers {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,13 +3,7 @@ INTO
   moments
   (title, img_Url, descripcion)
 VALUES
-  ('Paisatge nevat', 'https://www.shbarcelona.cat/blog/cat/wp-content/uploads/2019/01/INVIERNO-810x540.jpg', 'imatge d''un paisatge nevat ');
-INSERT
-INTO
-  moments
-  (title, img_Url, descripcion)
-VALUES
-  ('CinqueTerre', 'https://promoviatgesblog.files.wordpress.com/2019/10/3-2.png', 'cinqueTerre');
+  ('Paisatge nevat', 'https://www.shbarcelona.cat/blog/cat/wp-content/uploads/2019/01/INVIERNO-810x540.jpg', 'imatge d''un paisatge nevat');
 INSERT
 INTO
   moments
@@ -20,9 +14,11 @@ INSERT
 INTO
   moments
   (title, img_Url, descripcion)
+VALUES
+  ('Ayutthaya Tailàndia', 'https://promoviatgesblog.files.wordpress.com/2019/10/10.png', 'Tailàndia És un dels països més internacionals d’Àsia');
 INSERT
 INTO
   moments
   (title, img_Url, descripcion)
 VALUES
-  ('Ayutthaya Tailàndia', 'https://promoviatgesblog.files.wordpress.com/2019/10/10.png', 'Tailàndia És un dels països més internacionals d’Àsia');
+  ('Arbre tardor', 'https://pbs.twimg.com/media/B1L31G9IYAE8G-n.jpg:large', 'prat a la tardor');
